c64bf27aef60a6a17517df100bfff7f3daf34e13
Cristian <cristianfmunoz0@gmail.com>
2025-06-15 12:26:31 -0500

    Primer commit :D

---
+++ README.md
@@ -0,0 +1,3 @@
+# Guayabita en C++
+
+Juego de azar programado en C++, basado en el tradicional juego colombiano La Guayabita.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "*********************************\n";
+    cout << "*        LA GUAYABITA           *\n";
+    cout << "*   ---------------------       *\n";
+    cout << "*  ¡Apuesta   y  diviértete!    *\n";
+    cout << "*********************************\n";
+
+    return 0;
+}
